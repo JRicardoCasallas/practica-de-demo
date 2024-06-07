b3ecf7e88c584713af44d012d46c994efd4df04c
Jose Ricardo Casallas <josericardocasasllas11@gmail.com>
2024-06-07 17:13:31 -0500

    practica del demo

---
+++ demo.js
@@ -0,0 +1,22 @@
+var stringVarible1 = "Cualquier cosa en la variable 1 "
+var stringVarible2 = "Cualquier cosa en la variable 2 "
+var stringVarible3 = "Cualquier cosa en la variable 3 "
+ 
+var numberVariable1 = 4444
+var numberVariable2 = 44.44
+var numberVariable3 = -44.44
+
+var boolean1 = true
+var booleanf = false
+
+var nulo = null
+var indefinido = undefined
+
+
+console.log("hola mundo!")
+console.log(stringVarible1, stringVarible2, numberVariable3)
+console.log(numberVariable1)
+console.log("4444")
+console.log(booleanf)
+console.log(nulo)
+console.log(indefinido)
