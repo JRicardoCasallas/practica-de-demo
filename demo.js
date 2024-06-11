7fb267bbe101c1155435dacaf565b68d4a301b6b
Jose Ricardo Casallas <josericardocasasllas11@gmail.com>
2024-06-11 00:03:03 -0500

    Ejercicio demo

--- demo.js
+++ demo.js
@@ -1,23 +1,46 @@
-var stringVarible1 = "Cualquier cosa en la variable 1 "
-var stringVarible2 = "Cualquier cosa en la variable 2 "
-var stringVarible3 = "Cualquier cosa en la variable 3 "
+
+function miFuncion(nombre, apellido, edad){
+  
+console.log("el nombre de la persona es: ", nombre)
+console.log("el nombre de la persona es: ", apellido)
+console.log("el apellido de la persona es: ", edad)
+
+
+}
+miFuncion("william","morales", 22)
+miFuncion("rut","sepulveda", 22)
+miFuncion("leonardo","gonzales", 22)
+
+// var boolean1 = true
+// var booleanf = false
+
+// var nulo = null
+//  var indefinido = undefined
+//  console.log("holamundo" !== "holamundo")
+
+
+
+
+// var stringVarible1 = "Cualquier cosa en la variable 1 "
+// var stringVarible2 = "Cualquier cosa en la variable 2 "
+// var stringVarible3 = "Cualquier cosa en la variable 3 "
  
-var numberVariable1 = 4444
-var numberVariable2 = 44.44
-var numberVariable3 = -44.44
+// var numberVariable1 = 4444
+// var numberVariable2 = 44.44
+// var numberVariable3 = -44.44
 
-var boolean1 = true
-var booleanf = false
+// var boolean1 = true
+// var booleanf = false
 
-var nulo = null
-var indefinido = undefined
+// var nulo = null
+// var indefinido = undefined
 
 
-// console.log("hola mundo!")
-// console.log(stringVarible1, stringVarible2, numberVariable3)
-// console.log(numberVariable1)
-// console.log("4444")
-// console.log(booleanf)
-// console.log(nulo)
-// console.log(indefinido)
-console.log(typeof indefinido)
+// // console.log("hola mundo!")
+// // console.log(stringVarible1, stringVarible2, numberVariable3)
+// // console.log(numberVariable1)
+// // console.log("4444")
+// // console.log(booleanf)
+// // console.log(nulo)
+// // console.log(indefinido)
+// console.log(typeof boolean1)
